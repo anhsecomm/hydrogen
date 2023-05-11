aa85cd8d6d5e40558be095507bbe8a234424341a
anh chong <anh.chon@secomm.vn>
2023-05-11 10:25:27 +0700

    Fix

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,4 +1,4 @@
-import { json } from "@remix-run/node";
+import { json } from '@shopify/remix-oxygen';
 import { useLoaderData } from "@remix-run/react";
 
 import Layout from "../components/Layout";
